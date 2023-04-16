47aa3b16639d4c3956262a5094b23a982a270e23
Penelope Julia <penelopejvogel@gmail.com>
2023-04-16 12:17:08 +0200

    Concept

--- A04_ToDoList_Data/ToDoList.js
+++ A04_ToDoList_Data/ToDoList.js
@@ -1,2 +1,6 @@
 "use strict";
+var L04_ToDoList;
+(function (L04_ToDoList) {
+    window.addEventListener("load", handleLoad);
+})(L04_ToDoList || (L04_ToDoList = {}));
 //# sourceMappingURL=ToDoList.js.map
