f56571ae4ceda1e08c4996e1cf32351405891941
Penelope Julia <penelopejvogel@gmail.com>
2023-05-06 23:37:15 +0200

    Art.js

--- A08.1_Generative_Kunst/art.js
+++ A08.1_Generative_Kunst/art.js
@@ -2,6 +2,7 @@
 var L08GenerativeKunst;
 (function (L08GenerativeKunst) {
     // Quelle: https://kernhanda.github.io/tutorial-typescript-canvas-drawing/
+    // Quelle: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
     // When page loads trigger function handleLoad
     window.addEventListener("load", handleLoad);
     // Declare new variables
@@ -19,6 +20,7 @@ var L08GenerativeKunst;
         drawCircle();
         drawElipse();
         drawLine();
+        drawRectangle();
     }
     function drawCircle() {
         // Circle size varies from min to max
@@ -28,10 +30,9 @@ var L08GenerativeKunst;
         circle.fillStyle = "#1F686A";
         circle.strokeStyle = "#1F686A";
         // Randomize Circle-Size
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 6; i++) {
             let x = Math.random() * circle.canvas.width;
             let y = Math.random() * circle.canvas.height;
-            // Source-Inspo: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
             circle.beginPath();
             circle.arc(x, y, size, 0, 2 * Math.PI);
             circle.fill();
@@ -50,7 +51,6 @@ var L08GenerativeKunst;
         for (let i = 0; i < 10; i++) {
             let x = Math.random() * elipse.canvas.width;
             let y = Math.random() * elipse.canvas.height;
-            // Source-Inspo https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/ellipse
             elipse.beginPath();
             elipse.ellipse(x, y, size, 15, 5, 0, 360);
             elipse.fill();
@@ -58,17 +58,32 @@ var L08GenerativeKunst;
             elipse.stroke();
         }
     }
+    function drawRectangle() {
+        let rect = canvas.getContext("2d");
+        // Give Circle Color 
+        rect.fillStyle = "#16C310";
+        rect.strokeStyle = "#16C310";
+        // Randomize Rectangle-Size
+        for (let i = 0; i < 4; i++) {
+            let x = Math.random() * rect.canvas.width;
+            let y = Math.random() * rect.canvas.height;
+            rect.beginPath();
+            rect.rect(x, y, 150, 100);
+            rect.fill();
+            rect.closePath();
+            rect.stroke();
+        }
+    }
     function drawLine() {
         // Give Lines a color
         let color = ["#BB1818", "#9C0CE0", "#14982E"];
         //Randomize Line-Sizes
         for (let i = 0; i < color.length; i++) {
-            for (let index = 0; index < 50; index++) {
+            for (let index = 0; index < 4; index++) {
                 let a = randomNumber(0, canvas.width);
                 let b = randomNumber(0, canvas.height);
                 let c = randomNumber(0, canvas.width);
                 let d = randomNumber(0, canvas.height);
-                // Source: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
                 crc2.beginPath();
                 crc2.moveTo(a, b);
                 crc2.lineTo(c, d);
@@ -77,6 +92,7 @@ var L08GenerativeKunst;
             }
         }
     }
+    // Function Value of max and min are assigned
     function randomNumber(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
